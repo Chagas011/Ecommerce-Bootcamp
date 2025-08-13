14ba29770d95a18a242d87b770001fdb90c2b38e
Chagas011 <washington.chagas.9@hotmail.com>
2025-08-13 16:38:53 -0300

    feat:add number and email a footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,19 @@
 export function Footer() {
   return (
-    <div className="bg-accent mt-5 w-full gap-1 p-8">
-      <p className="text-xs font-medium">© 2025 Copyright BEWEAR</p>
-      <p className="text-muted-foreground text-xs font-medium">
-        Todos os direitos reservados
-      </p>
+    <div className="bg-accent mt-5 flex w-full justify-between gap-1 p-8">
+      <div>
+        <p className="text-xs font-medium">
+          © 2025 Copyright Washington Chagas
+        </p>
+        <p className="text-muted-foreground text-xs font-medium">
+          Todos os direitos reservados
+        </p>
+      </div>
+
+      <div>
+        <p>Celular: (11) 98500-1474</p>
+        <p>Email: washington.chagas.9@hotmail.com</p>
+      </div>
     </div>
   );
 }
